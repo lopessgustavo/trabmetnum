1fa8169659e74c46e9a6ffcb7845bb170d92371b
gustavo <gustavolp@id.uff.br>
2019-05-29 22:38:05 -0300

    eliminacao de gauss ok

--- trab.py
+++ trab.py
@@ -5,19 +5,37 @@ def zerarColuna(matriz,linha):
         
         #subtrair linhas
         for j in range(coluna,len(matriz)):
-            matriz[i][j] -= matriz[linha][coluna]*multiplicador
+            matriz[i][j] -= matriz[linha][j]*multiplicador
+
+def imprime(matriz):
+    for linha in matriz:
+        print(linha)
+
+def pivotear(matriz,coluna):
+    linha = coluna
+    maior = coluna
+    for i in range(linha, len(matriz)):
+        if abs(matriz[i][coluna]) > matriz[maior][coluna]:
+            maior = i
+    matriz[linha], matriz[maior] = matriz[maior],matriz[linha]
 
 
 def eliminacaoGauss(matriz):
     # verificarDiagonalPrincipal(matriz)
     
     for linha in range(len(matriz)):
+        pivotear(matriz,linha)
         zerarColuna(matriz,linha)
 
 if __name__ == '__main__':
-    linha,coluna = [int(x) for x in input().split(" ")]
-    matriz = []
-    for i in range(linha):
-        linha = []
-        
+    matriz =    [[2,-1,4,0],
+                [4,-1,5,1],
+                [-2,2,-2,3],
+                [0,3,-9,4]]
+    eliminacaoGauss(matriz)
+    imprime(matriz)
+    # tam = int(input())
+    # matriz = []W
+    # for i in range(tam):
+    #     linha =         
             
